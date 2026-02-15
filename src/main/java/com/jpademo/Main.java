9075d3441f80583534b36c3515cd7f327febe4ec
HungNM <lalala123hung@gmail.com>
2026-02-15 08:26:49 +0700

    update leetcdoe

--- src/main/java/com/jpademo/Main.java
+++ src/main/java/com/jpademo/Main.java
@@ -7,36 +7,44 @@ import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int m;
+//        m = sc.nextInt();
+//        int[] nums = new int[m];
+//        for(int i = 0; i< m; i++){
+//            nums[i] = sc.nextInt();
+//        }
+//        int n = nums.length;
+//        List<String> list = new ArrayList<>();
+//        int j = 0;
+//        for(int i = 0; i<n-1; i++){
+//            if( nums[i + 1] - nums[i] != 1){
+//                if( nums[i] == nums[j]){
+//                    list.add(String.valueOf(nums[j]));
+//                }else{
+//                    list.add(nums[j] + "->" + nums[i]);
+//                }
+//
+//                j = i + 1;
+//            }
+//        }
+//
+//        if(n > 0){
+//            if(nums[n - 1] == nums[j]){
+//                list.add(String.valueOf(nums[j]));
+//            }else{
+//                list.add(nums[j] + "->" + nums[n - 1]);
+//            }
+//        }
+//    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int m;
-        m = sc.nextInt();
-        int[] nums = new int[m];
-        for(int i = 0; i< m; i++){
-            nums[i] = sc.nextInt();
-        }
-        int n = nums.length;
-        List<String> list = new ArrayList<>();
-        int j = 0;
-        for(int i = 0; i<n-1; i++){
-            if( nums[i + 1] - nums[i] != 1){
-                if( nums[i] == nums[j]){
-                    list.add(String.valueOf(nums[j]));
-                }else{
-                    list.add(nums[j] + "->" + nums[i]);
-                }
 
-                j = i + 1;
-            }
-        }
+        String s1 = sc.nextLine();
+        String s2 = sc.nextLine();
 
-        if(n > 0){
-            if(nums[n - 1] == nums[j]){
-                list.add(String.valueOf(nums[j]));
-            }else{
-                list.add(nums[j] + "->" + nums[n - 1]);
-            }
-        }
     }
 
 }
@@ -75,4 +83,26 @@ class Solution {
 }
  */
 
-//sdfsdf
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
